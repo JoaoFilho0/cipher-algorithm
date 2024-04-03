001ebd4d943b0f988175adcf71f39a8c6c8083f9
João  Filho <joao.filho@darmlabs.ifce.edu.br>
2024-04-02 21:58:50 -0300

    test prints removed

--- cipher_algorithm.py
+++ cipher_algorithm.py
@@ -4,7 +4,6 @@ import functions_cript
 actionTaken = input("Deseja criptografar(cript) ou descriptografar(decript)?: ")
 jumps = int(input("Chave(1-25): "))
 text = ''.join(ch for ch in unicodedata.normalize('NFKD', input("Texto a ser utilizado: ")) if not unicodedata.combining(ch))
-print(text)
 finalText = ""
 
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
