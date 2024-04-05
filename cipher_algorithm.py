2affcc189bcccf9c1a2b9204cd96a777fa6f08de
João  Filho <joao.filho@darmlabs.ifce.edu.br>
2024-04-05 12:09:01 -0300

    new decrypt option added

--- cipher_algorithm.py
+++ cipher_algorithm.py
@@ -1,8 +1,16 @@
 import unicodedata
 import functions_cript
 
-actionTaken = input("Deseja criptografar(cript) ou descriptografar(decript)?: ")
-jumps = int(input("Chave(1-25): "))
+actionTaken = input("Deseja criptografar(cript) ou descriptografar(decript): ")
+if actionTaken == "decript":
+    response = input("Possui a chave? (s ou n): ")
+    if  response == "s":
+        jumps = int(input("Chave(1-25): "))
+    elif response == "n":
+        jumps = 0
+elif actionTaken == "cript":
+    jumps = int(input("Chave(1-25): "))
+
 text = ''.join(ch for ch in unicodedata.normalize('NFKD', input("Texto a ser utilizado: ")) if not unicodedata.combining(ch))
 finalText = ""
 
@@ -17,18 +25,35 @@ if actionTaken == "cript":
         else:
             finalText += " "
 
-    print(finalText)
+    print("Texto criptografado: " + finalText)
 
 elif actionTaken == "decript":
     textArrayChar = [char for char in text]
-    
-    for textChar in textArrayChar:
-        if textChar != " ":
-            finalText += functions_cript.decriptChar(alphabet, functions_cript.getCharIndex(textChar, alphabet), jumps) if textChar.islower() else str.upper(functions_cript.decriptChar(alphabet, functions_cript.getCharIndex(textChar, alphabet), jumps))
-        else:
-            finalText += " "
+    aux = ""
 
-    print(finalText)
+    if jumps != 0:
+        for textChar in textArrayChar:
+            if textChar != " ":
+                finalText += functions_cript.decriptChar(alphabet, functions_cript.getCharIndex(textChar, alphabet), jumps) if textChar.islower() else str.upper(functions_cript.decriptChar(alphabet, functions_cript.getCharIndex(textChar, alphabet), jumps))
+            else:
+                finalText += " "
+    else:
+        finalText = []
+        for num in range(1,25):
+            for textChar in textArrayChar:
+                if textChar != " ":
+                    aux += functions_cript.decriptChar(alphabet, functions_cript.getCharIndex(textChar, alphabet), num) if textChar.islower() else str.upper(functions_cript.decriptChar(alphabet, functions_cript.getCharIndex(textChar, alphabet), num))
+                else:
+                    aux += " "
+            
+            finalText.append(aux)
+            aux = ""
+    
+    print("Texto descriptografado: ")
+    if type(finalText) == str:
+        print(finalText)
+    else:
+        print("\n",*finalText, sep = "\n")
     
 else:
     print("Por favor, informe se deseja criptografar(cript) ou descriptografar(decript).")
--- functions_cript.py
+++ functions_cript.py
@@ -2,7 +2,7 @@ def criptChar(alphabet: list, charIndex: int, jumps: int) -> str:
     return str.lower(alphabet[((charIndex + jumps) - 26) if ((charIndex + jumps) > 25) else (charIndex + jumps)])
 
 def decriptChar(alphabet: list, charIndex: int, jumps: int) -> str:
-    return str.lower(alphabet[((charIndex - jumps) + 26) if ((charIndex - jumps) < 1) else (charIndex - jumps)])
+    return str.lower(alphabet[((charIndex - jumps) + 26) if ((charIndex - jumps) < 0) else (charIndex - jumps)])
 
 def getCharIndex(textChar: str, alphabet: list) -> int:
     for index, item in enumerate(alphabet):
